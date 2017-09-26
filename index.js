81f742c3e10206c9cd54d93abe75635c9df18e73
Peter Weinberg <pweinberg633@gmail.com>
2017-09-25 23:08:24 -0400

    implement game loop and in-game commands

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ console.log(
         figlet.textSync('Battleship CLI', {
             font: 'graffiti',
             horizontalLayout: 'full'
-        })
+        }),
+        '\n'
     )
 );
 
@@ -26,64 +27,82 @@ const game = new Game();
 
 /** TODO:
     // * Handle player entering invalid coords - right now they lose a turn
-    * Provide interface for setting up ships
+    // * Provide interface for setting up ships
     * Fix spacing
     * Add appropriate colors to hit, miss, and sunk messages
-    * Give options to check score and view ownBord during gameplay
-    * Add "ready to play?" prompt that will initialize game and push to the next prompt
+    * Configure end of game / reset prompt
+    * import messages from util file
+    * move isCoordinateValid to util file
+    // * Give options to check score and view ownBord during gameplay
+    // * Add "ready to play?" prompt that will initialize game and push to the next prompt
     * Set random first player? If so, need way to allow playerOne to configure ships before cpu takes turn
+    * enable difficulty settings (size of board?, cpu makes wild guess every time)
 */
 
-function initializeGame() {
-    const questions = [
+function initializeGame(callback) {
+    const question = [
         {
             type: 'input',
             name: 'ready',
-            message: 'Welcome to Battleship CLI! Press enter to continue.',
-            validate: function(val) {
-                return true;
-            }
+            message: 'Welcome to Battleship CLI! Press enter to continue.'
         }
     ];
 
-    inquirer.prompt(questions).then(function() {
-        game.setInitialState();
-        playerOneConfigureShips(['Cruiser', 'Submarine', 'Battleship', 'Destroyer', 'Carrier']);
-    });
+    inquirer.prompt(question).then(callback);
 }
 
 function playerOneConfigureShips(ships) {
     var message =
         'Place a ship! e.g. cruiser b3 right' +
         (ships.length === 5 ? '\n  Use coordinates A1-J10 and left, right, up or down\n' : '\n') +
-        `  Ships remaining: ${JSON.stringify(ships)}`;
+        `  Ships remaining [type, size]: ${JSON.stringify(ships).replace(/"/g, "'")}`;
 
-    const questions = [
+    const question = [
         {
             type: 'input',
             name: 'placeShip',
-            message,
-            // default: '',
+            message: message,
             validate: function(instruction) {
-                const [ ship, coord, direction ] = instruction.split(' ');
-                var message = game.playerOne.placeShip(ship, coord, direction);
-                if (message) {
-                    return message;
-                } else {
-                    return true;
+                switch (instruction) {
+                    case 'show board':
+                        return game.playerOne.showBoard();
+                    case 'show score':
+                        return game.status;
+                    case 'q':
+                    case 'quit':
+                        console.log('\n\nGoodbye...');
+                        process.exit();
+
+                    default:
+
+                    var [ ship, coords, direction ] = instruction.split(' ');
+                    // if ship placement is successful,
+                    // playerOne.placeShip returns undefined
+                    var message = game.playerOne.placeShip(
+                        ship.toLowerCase(),
+                        coords.toUpperCase(),
+                        direction.toLowerCase()
+                    );
+                    // return error message or continue
+                    return message ? message : true;
                 }
             }
         }
     ];
     // recurse until all ships are placed
-    inquirer.prompt(questions).then(function() {
-        if (game.confirmInitialization()) {
-            takeTurn();
+    inquirer.prompt(question).then(function() {
+        if (game.playerOneReady()) {
+            game.setInitialState();
+            startGame();
         } else {
             ships = [];
-            for (var ship of game.playerOne.ships) {
+            for (let ship of game.playerOne.ships) {
                 if (!ship.placed) {
-                    ships.push(ship.type.slice(0,1).toUpperCase() + ship.type.slice(1));
+                    ships.push(
+                        ship.type.slice(0,1).toUpperCase() +
+                        ship.type.slice(1) + ', ' +
+                        ship.size
+                    );
                 }
             }
             playerOneConfigureShips(ships);
@@ -91,38 +110,105 @@ function playerOneConfigureShips(ships) {
     })
 }
 
-function takeTurn() {
-    const questions = [
+function startGame() {
+    const question = [
         {
             type: 'input',
-            name: 'move',
+            name: 'ready',
+            message: `Ready to play? ${game.message} (press enter to continue)`,
+            validate: function(val) {
+                return true;
+            }
+        }
+    ];
+
+    inquirer.prompt(question).then(function() {
+        if (!game.coinToss) {
+            setTimeout(() => game.attack(), 500);
+            setTimeout(() => takeTurn(), 900);
+        } else {
+            takeTurn();
+        }
+    })
+}
+
+function takeTurn() {
+    if (game.gameOver) {
+        return gameOver();
+    }
+
+    const question = [
+        {
+            type: 'input',
+            name: 'coords',
             message: 'Take a guess! Enter coordinates A1-J10, e.g. B7:',
             validate: function(coords) {
-                if (game.playerOne.isCoordinateValid(coords)) {
-                    return true;
-                } else {
-                    return "Please enter valid coordinates"
+                switch (coords) {
+                    case 'show board':
+                        return game.playerOne.showBoard();
+                    case 'show score':
+                        return game.status;
+                    case 'q':
+                    case 'quit':
+                        console.log('\n\nGoodbye...');
+                        process.exit();
+
+                    default:
+
+                    if (game.playerOne.isCoordinateValid(coords)) {
+                        return true;
+                    } else {
+                        return "Please enter valid coordinates"
+                    }
                 }
             }
         }
     ];
 
-    inquirer.prompt(questions).then(function(answer) {
-        game.attack(answer.move);
-        setTimeout(() => game.attack(), 1000);
+    inquirer.prompt(question).then(function(move) {
+        game.attack(move.coords);
+        setTimeout(() => game.attack(), 800);
         setTimeout(() => takeTurn(), 1200);
     });
 }
 
-// takeTurn();
-initializeGame();
+function gameOver() {
+    const question = [
+        {
+            type: 'confirm',
+            name: 'newGame',
+            message: game.message
+        }
+    ];
 
-
-
-
-
-
-function randomizeCoordinates() {
-    const rows = ['A','B','C','D','E','F','G','H','I','J'];
-    return rows[Math.floor(Math.random() * 10)] + Math.ceil(Math.random() * 10);
+    inquirer.prompt(question).then(function(answer) {
+        if (answer.newGame) {
+            game.reset();
+            game.populateP1Ships();
+            console.log('Ready? Here we go again...');
+            setTimeout(() => {
+                playerOneConfigureShips([
+                    'Cruiser, 3',
+                    'Submarine, 3',
+                    'Battleship, 4',
+                    'Destroyer, 2',
+                    'Carrier, 5'
+                ]);
+            }, 1000);
+        } else {
+            console.log('\n\nThanks for playing Battleship CLI! Goodbye!\n\n');
+            process.exit();
+        }
+    });
 }
+
+initializeGame(function() {
+    game.populateP1Ships();
+    playerOneConfigureShips([
+        'Cruiser, 3',
+        'Submarine, 3',
+        'Battleship, 4',
+        'Destroyer, 2',
+        'Carrier, 5'
+    ]);
+});
